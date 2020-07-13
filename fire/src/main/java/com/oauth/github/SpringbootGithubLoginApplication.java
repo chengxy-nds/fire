e22d0fed89ce2af293d7b3fd1fe0e631156ae684
xin <515361725@qq.com>
2020-07-13 18:34:02 +0800

    fire增加GitHub授权登录功能

---
+++ fire/src/main/java/com/oauth/github/SpringbootGithubLoginApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubLoginApplication.class, args);
+	}
+
+}
