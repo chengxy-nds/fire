b826dbe97fb97daf057fa3e6261d7d39cdf3a5ce
xin <515361725@qq.com>
2020-07-13 18:29:21 +0800

    fire增加GitHub授权登录功能

---
+++ fire/src/main/java/com/oauth/github/controller/LoginController.java
@@ -0,0 +1,88 @@
+package com.oauth.github.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.oauth.github.config.GitHubProperties;
+import com.oauth.github.pojo.UserInfo;
+import com.oauth.github.util.OkHttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @Author: xinzhifu
+ * @Description:
+ */
+@Slf4j
+@Controller
+public class LoginController {
+
+
+    @Autowired
+    private GitHubProperties gitHubProperties;
+
+    /**
+     * @param code
+     * @author xiaofu
+     * @description 授权回调
+     * @date 2020/7/10 15:42
+     */
+    @RequestMapping("/authorize/redirect")
+    public ModelAndView authorize(@NotEmpty String code) {
+
+        log.info("授权码code: {}", code);
+
+        /**
+         * 重新到前端主页
+         */
+        String redirectHome = "http://127.0.0.1:8080/home";
+
+        try {
+            /**
+             * 1、拼装获取accessToken url
+             */
+            String accessTokenUrl = gitHubProperties.getAccesstokenUrl()
+                    .replace("clientId", gitHubProperties.getClientId())
+                    .replace("clientSecret", gitHubProperties.getClientSecret())
+                    .replace("authorize_code", code);
+
+            /**
+             * 返回结果中直接返回token
+             */
+            String result = OkHttpClientUtil.sendByGetUrl(accessTokenUrl);
+            log.info(" 请求 token 结果：{}", result);
+
+            String accessToken = null;
+            Pattern p = Pattern.compile("=(\\w+)&");
+            Matcher m = p.matcher(result);
+            while (m.find()) {
+                accessToken = m.group(1);
+                log.info("令牌token：{}", m.group(1));
+                break;
+            }
+
+            /**
+             * 成功获取token后，开始请求用户信息
+             */
+            String userInfoUrl = gitHubProperties.getUserUrl().replace("accessToken", accessToken);
+
+            String userResult = OkHttpClientUtil.sendByGetUrl(userInfoUrl);
+
+            log.info("用户信息：{}", userResult);
+
+            UserInfo userInfo = JSON.parseObject(userResult, UserInfo.class);
+
+            redirectHome += "?name=" + userInfo.getName();
+
+        } catch (Exception e) {
+            log.error("授权回调异常={}", e);
+        }
+        return new ModelAndView(new RedirectView(redirectHome));
+    }
+}
