35d26d75ff50a2ed6fb4f459548594ef6a579d35
xin <515361725@qq.com>
2020-07-23 17:46:03 +0800

    新增common模块

---
+++ fire/fire-repository/src/test/java/com/fire/repository/FireRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fire.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
